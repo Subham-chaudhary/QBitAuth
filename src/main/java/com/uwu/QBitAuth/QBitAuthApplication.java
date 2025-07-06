ae58a181d3f7dd21688d90142dfc1622a41f57a1
subham-chaudhary <shubhu.uwu@gmail.com>
2025-07-06 16:04:04 +0530

    Spring Service Init

---
+++ src/main/java/com/uwu/QBitAuth/QBitAuthApplication.java
@@ -0,0 +1,13 @@
+package com.uwu.QBitAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QBitAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QBitAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uwu/QBitAuth/QBitAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwu.QBitAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QBitAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
